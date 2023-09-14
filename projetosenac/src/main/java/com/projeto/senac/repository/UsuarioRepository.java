e361c8d19cf75440e72518b1a966996ba88dc531
JoaoVicSO <104661027+JoaoVicSO@users.noreply.github.com>
2023-09-14 10:34:24 -0300

    Add files via upload

---
+++ projetosenac/src/main/java/com/projeto/senac/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.projeto.senac.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.projeto.senac.model.Usuario;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+
+}
