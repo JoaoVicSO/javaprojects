e980667f4bb7fe15b4960ae8f0f24ac90bdd6442
JoaoVicSO <104661027+JoaoVicSO@users.noreply.github.com>
2023-09-14 12:00:48 -0300

    Delete projetosenac directory

--- projetosenac/src/main/java/com/projeto/senac/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.senac.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.projeto.senac.model.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
-
-}
